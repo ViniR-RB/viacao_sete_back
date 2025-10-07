7776fcecda3eae695e63423a6dcf3d2a30f5616f
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-10-07 12:03:22 -0300

    feat: add module for pagination example

---
+++ src/modules/pagination/domain/entities/page.entity.ts
@@ -0,0 +1,32 @@
+import PageMetaEntity from '@/modules/pagination/domain/entities/page_meta.entity';
+
+export default class PageEntity<T> {
+  constructor(
+    private readonly data: T[],
+    private meta: PageMetaEntity,
+  ) {
+    this.data = data;
+    this.meta = meta;
+  }
+
+  get pageData() {
+    return this.data;
+  }
+
+  toObject() {
+    return {
+      data: this.data.map(item => {
+        if (
+          item !== null &&
+          typeof item === 'object' &&
+          'toObject' in item &&
+          typeof item.toObject === 'function'
+        ) {
+          return item.toObject();
+        }
+        return item;
+      }),
+      meta: this.meta,
+    };
+  }
+}
