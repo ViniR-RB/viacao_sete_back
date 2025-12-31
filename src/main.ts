d0deb838e463672f0cac2f012f8ccf3c02e43bc1
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2025-12-31 12:16:26 -0300

    feat: create api json

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import ConfigurationService from '@/core/services/configuration.service';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,16 +13,19 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  const options = new DocumentBuilder()
-    .setTitle('Base APi')
-    .setDescription('Base Api')
-    .setVersion('1.0')
-    .addServer('http://localhost:3000/', 'DEV')
-    .addTag('Base Api')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
+  const config = app.get(ConfigurationService);
+  if (config.get('NODE_ENV') === 'dev') {
+    const options = new DocumentBuilder()
+      .setTitle('Base APi')
+      .setDescription('Base Api')
+      .setVersion('1.0')
+      .addServer('http://localhost:3000/', 'DEV')
+      .addTag('Base Api')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
+    SwaggerModule.setup('docs', app, document);
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
