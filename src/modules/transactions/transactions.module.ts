448cfc02cc042fc17390c9af4b2f12722076f2ca
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2026-02-02 12:47:36 -0300

    feat: register new dependencies for payment

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -2,32 +2,43 @@ import CoreModule from '@/core/core_module';
 import IUnitOfWork from '@/core/interface/i_unit_of_work';
 import { UNIT_OF_WORK } from '@/core/symbols';
 import AuthModule from '@/modules/auth/auth.module';
+import IPaymentMethodRepository from '@/modules/transactions/adapters/i_payment_method.repository';
 import ITransactionCategoryRepository from '@/modules/transactions/adapters/i_transaction_category.repository';
+import CreatePaymentMethodService from '@/modules/transactions/application/create_payment_method.service';
 import CreateTransactionService from '@/modules/transactions/application/create_transaction.service';
 import CreateTransactionCategoryService from '@/modules/transactions/application/create_transaction_category.service';
+import DeletePaymentMethodService from '@/modules/transactions/application/delete_payment_method.service';
 import DeleteTransactionService from '@/modules/transactions/application/delete_transaction.service';
 import DeleteTransactionCategoryService from '@/modules/transactions/application/delete_transaction_category.service';
 import ExtractTransactionSummaryService from '@/modules/transactions/application/extract_transaction_summary.service';
+import ListPaymentMethodsService from '@/modules/transactions/application/list_payment_methods.service';
 import ListTransactionCategoriesService from '@/modules/transactions/application/list_transaction_categories.service';
 import ListTransactionsService from '@/modules/transactions/application/list_transactions.service';
 import UpdateTransactionService from '@/modules/transactions/application/update_transaction.service';
 import UpdateTransactionCategoryService from '@/modules/transactions/application/update_transaction_category.service';
+import PaymentMethodController from '@/modules/transactions/controller/payment-method.controller';
 import TransactionsController from '@/modules/transactions/controller/transactions.controller';
 import TransactionCreationDomainService from '@/modules/transactions/domain/services/transaction_creation.domain_service';
+import PaymentMethodModel from '@/modules/transactions/infra/models/payment-method.model';
 import TransactionCategoryModel from '@/modules/transactions/infra/models/transaction-category.model';
 import TransactionModel from '@/modules/transactions/infra/models/transaction.model';
 import TransactionLineDetailsModel from '@/modules/transactions/infra/models/transaction_line_details.model';
+import PaymentMethodRepository from '@/modules/transactions/infra/repositories/payment-method.repository';
 import TransactionCategoryRepository from '@/modules/transactions/infra/repositories/transaction-category.repository';
 import TransactionRepository from '@/modules/transactions/infra/repositories/transaction.repository';
 import TransactionLineDetailsRepository from '@/modules/transactions/infra/repositories/transaction_line_details.repository';
 import {
+  CREATE_PAYMENT_METHOD_SERVICE,
   CREATE_TRANSACTION_CATEGORY_SERVICE,
   CREATE_TRANSACTION_SERVICE,
+  DELETE_PAYMENT_METHOD_SERVICE,
   DELETE_TRANSACTION_CATEGORY_SERVICE,
   DELETE_TRANSACTION_SERVICE,
   EXTRACT_TRANSACTION_SUMMARY_SERVICE,
+  LIST_PAYMENT_METHODS_SERVICE,
   LIST_TRANSACTION_CATEGORIES_SERVICE,
   LIST_TRANSACTIONS_SERVICE,
+  PAYMENT_METHOD_REPOSITORY,
   TRANSACTION_CATEGORY_REPOSITORY,
   TRANSACTION_CREATION_DOMAIN_SERVICE,
   TRANSACTION_LINE_DETAILS_REPOSITORY,
@@ -45,11 +56,12 @@ import { Repository } from 'typeorm';
       TransactionModel,
       TransactionCategoryModel,
       TransactionLineDetailsModel,
+      PaymentMethodModel,
     ]),
     AuthModule,
     CoreModule,
   ],
-  controllers: [TransactionsController],
+  controllers: [TransactionsController, PaymentMethodController],
   providers: [
     {
       inject: [getRepositoryToken(TransactionModel)],
@@ -68,6 +80,12 @@ import { Repository } from 'typeorm';
       provide: TRANSACTION_LINE_DETAILS_REPOSITORY,
       useFactory: (repo: Repository<TransactionLineDetailsModel>) =>
         new TransactionLineDetailsRepository(repo),
+    },
+    {
+      inject: [getRepositoryToken(PaymentMethodModel)],
+      provide: PAYMENT_METHOD_REPOSITORY,
+      useFactory: (repo: Repository<PaymentMethodModel>) =>
+        new PaymentMethodRepository(repo),
     },
     {
       inject: [TRANSACTION_LINE_DETAILS_REPOSITORY],
@@ -142,6 +160,24 @@ import { Repository } from 'typeorm';
       provide: EXTRACT_TRANSACTION_SUMMARY_SERVICE,
       useFactory: (transactionRepository: TransactionRepository) =>
         new ExtractTransactionSummaryService(transactionRepository),
+    },
+    {
+      inject: [PAYMENT_METHOD_REPOSITORY],
+      provide: CREATE_PAYMENT_METHOD_SERVICE,
+      useFactory: (paymentMethodRepository: IPaymentMethodRepository) =>
+        new CreatePaymentMethodService(paymentMethodRepository),
+    },
+    {
+      inject: [PAYMENT_METHOD_REPOSITORY],
+      provide: LIST_PAYMENT_METHODS_SERVICE,
+      useFactory: (paymentMethodRepository: IPaymentMethodRepository) =>
+        new ListPaymentMethodsService(paymentMethodRepository),
+    },
+    {
+      inject: [PAYMENT_METHOD_REPOSITORY],
+      provide: DELETE_PAYMENT_METHOD_SERVICE,
+      useFactory: (paymentMethodRepository: IPaymentMethodRepository) =>
+        new DeletePaymentMethodService(paymentMethodRepository),
     },
   ],
 })
